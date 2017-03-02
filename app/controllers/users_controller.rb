d4031682da44e72731b02f5175252b9fdbbf3b8a
authorhuynh <hung1972005@gmail.com>
2017-03-02 10:31:39 +0700

    Sign up and login logout

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,31 @@
+class UsersController < ApplicationController
+
+  def show
+    @user = User.find_by id: params[:id]
+    unless @user
+      flash[:warning] = t "view.users.show.error_user"
+      redirect_to login_path
+    end
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new user_params    
+    if @user.save
+      log_in @user
+      flash[:success] = t "view.users.news.msg_success"
+      redirect_to @user
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit :name, :email, :password, :password_confirmation
+  end
+end
